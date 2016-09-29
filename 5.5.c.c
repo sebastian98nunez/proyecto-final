b6602482a816c06cc427433ae21094bd3fb71465
sebastian98nunez <chatonunez98@hotmail.com>
2016-09-29 09:53:43 -0500

    Update 5.5.c.c

--- 5.5.c.c
+++ 5.5.c.c
@@ -4,12 +4,12 @@
 //devuelve menor a 0 si es mayor, 0 si son iguales y menor a 0 si es menor
 
 int strncmp(char *s,char *t,int n){
-    for(;n!=0&&*s==*t;s++,t++){
+    for(;n!=0 && *s==*t;s++,t++){ //compara  s y t hasta el final
         if(*s=='\0')
             return 0;
         n--;
     }
-    return *s-*t;
+    return *s-*t; 
 }
 
 int main()
